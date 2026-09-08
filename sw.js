ee02f18b23b2a2950f79d6385ca1494794a3a9ed
metusalem969-ro <sflorinciprian@yahoo.com>
2026-09-08 13:00:13 +0100

    Update cache version to v4 in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service worker: dupa prima incarcare, pagina merge complet fara internet. */
-const CACHE = "vitezometru-v3";
+const CACHE = "vitezometru-v4";
 const FILES = [
   "./",
   "./index.html",
