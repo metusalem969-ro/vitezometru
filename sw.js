2772b535c633b3f28ee1e1bac651d6b387f1e0a2
metusalem969-ro <sflorinciprian@yahoo.com>
2026-09-08 12:00:52 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+/* Service worker: dupa prima incarcare, pagina merge complet fara internet. */
+const CACHE = "vitezometru-v3";
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// cache-first: nu atinge reteaua daca fisierul e deja salvat
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      if (res && res.ok && res.type === "basic") {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+      }
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
